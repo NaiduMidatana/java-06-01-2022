71757dd1ec3762fcda30c5181c302d0ebb89b3c3
NaiduMidatana <96723183+NaiduMidatana@users.noreply.github.com>
2022-01-06 11:07:36 +0530

    using of extends keyword

---
+++ ContractEmployee.java
@@ -0,0 +1,10 @@
+package Inheritance;
+
+public class ContractEmployee extends Employee {
+
+	String WorkingHours;
+	 
+	void CalculateSalary() {
+	  System.out.println("Employee salary= " + Salary);
+	}
+}
---
+++ EmployeeManagement.java
@@ -0,0 +1,14 @@
+package Inheritance;
+
+public class EmployeeManagement {
+
+	public static void main(String[] args) {
+	
+		ContractEmployee conemp = new ContractEmployee();
+      
+	      conemp.enterEmployeeDetails();
+	      conemp.CalculateSalary();
+	      conemp.viewEmployeeDetails(); 
+	}
+
+}
